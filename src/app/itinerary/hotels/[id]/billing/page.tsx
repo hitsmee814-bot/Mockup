16437e0ffe46d08caa8a12a91b10a53593df6c6e
Rahul <hitsmee814@gmail.com>
2026-03-22 12:19:32 +0530

    Hotel added

--- src/app/itinerary/hotels/[id]/billing/page.tsx
+++ src/app/itinerary/hotels/[id]/billing/page.tsx
@@ -1,7 +1,19 @@
 import { HotelBilling } from "@/app/components/hotel-booking"
+import { hotels } from "@/app/components/hotel-booking/data"
 import { use } from "react"
 
-export default function HotelBillingPage({ params }: { params: Promise<{ id: string }> }) {
+export function generateStaticParams() {
+  return hotels.map((hotel) => ({
+    id: hotel.id.toString(),
+  }))
+}
+
+export default function HotelBillingPage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
   const { id } = use(params)
+
   return <HotelBilling id={id} />
 }
