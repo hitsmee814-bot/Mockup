807ab5479eaff6f0955981c68d43cc6b3171854b
Rahul <hitsmee814@gmail.com>
2026-03-22 12:22:19 +0530

    Hotel added

--- src/app/itinerary/hotels/[id]/page.tsx
+++ src/app/itinerary/hotels/[id]/page.tsx
@@ -1,10 +1,17 @@
 import { HotelDetail } from "@/app/components/hotel-booking"
+import { hotels } from "@/app/components/hotel-booking/data"
 
-export default async function HotelDetailPage({
-  params,
-}: {
+export function generateStaticParams() {
+  return hotels.map(hotel => ({
+    id: hotel.id.toString(),
+  }))
+}
+
+interface Props {
   params: Promise<{ id: string }>
-}) {
+}
+
+export default async function HotelDetailPage({ params }: Props) {
   const { id } = await params
 
   return <HotelDetail id={id} />
